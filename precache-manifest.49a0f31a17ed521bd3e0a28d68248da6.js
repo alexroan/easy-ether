217d9d9d82c3c1d0c8c785e17b2d13f471c394da
Alex Roan <alex.roan@hotmail.com>
2020-05-18 15:10:28 +0100

    Updates

---
+++ precache-manifest.49a0f31a17ed521bd3e0a28d68248da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c60f6836115b3c7937a63f1bc0c11d",
+    "url": "/easy-ether/index.html"
+  },
+  {
+    "revision": "de75757e32f68bbe9e1c",
+    "url": "/easy-ether/static/css/main.c514adf4.chunk.css"
+  },
+  {
+    "revision": "2c8d51523d4924dd7865",
+    "url": "/easy-ether/static/js/2.25a31cdc.chunk.js"
+  },
+  {
+    "revision": "08b176aa8cea4d6aaf4af871f5efe2e5",
+    "url": "/easy-ether/static/js/2.25a31cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de75757e32f68bbe9e1c",
+    "url": "/easy-ether/static/js/main.01f155c2.chunk.js"
+  },
+  {
+    "revision": "13a9768dc313cacfee47",
+    "url": "/easy-ether/static/js/runtime-main.194e23fe.js"
+  }
+]);
