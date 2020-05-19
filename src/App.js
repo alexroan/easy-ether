502d78a89e616621af9a7eb9e44599970ed4d4cc
Alex Roan <alex.roan@hotmail.com>
2020-05-19 10:25:19 +0100

    Added social links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.scss';
 import { balanceSelector } from './redux/selectors';
 import Account from './Account';
 import Login from './Login';
+import { Container, Row, Col } from 'react-bootstrap';
+import {SocialIcon} from 'react-social-icons';
 
 class App extends Component {
 
@@ -14,6 +16,23 @@ class App extends Component {
 		return (
 		<div className="app h-100">
 			{ loadedBalance != null ? <Account /> : <Login /> }
+			<footer>
+				<Container>
+					<Row>
+						<Col className="text-center pt-4 pb-2">
+							<span><a href="https://alexroan.co.uk">Alex Roan</a></span>
+						</Col>
+					</Row>
+					<Row>
+						<Col className="text-center pb-4">
+							<SocialIcon className="mx-1" url="https://twitter.com/alexroan" />
+							<SocialIcon className="mx-1" url="https://github.com/alexroan" />
+							<SocialIcon className="mx-1" url="https://medium.com/@alexroan" />
+							<SocialIcon className="mx-1" network="email" url="alex.roan@hotmail.com" />
+						</Col>
+					</Row>
+				</Container>
+			</footer>
 		</div>
 		);
 	}
