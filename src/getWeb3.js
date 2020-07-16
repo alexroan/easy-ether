17f54c58a2911e220acbcc7d5928f11d5e624846
Alex Roan <alex.roan@hotmail.com>
2020-07-16 17:43:14 +0100

    Revert "Removed fortmatic whilst domain problem not resolved"
    This reverts commit fe311c2382e99bc581e46df8e25b6890de430822.

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 import Portis from "@portis/web3";
 import Torus from "@toruslabs/torus-embed";
 import Authereum from "authereum";
-// import Fortmatic from "fortmatic";
+import Fortmatic from "fortmatic";
 import Web3Modal from "web3modal";
 
 export const getWeb3 = async () => {
@@ -25,12 +25,12 @@ export const getWeb3 = async () => {
 				}
 			}
 		},
-		// fortmatic: {
-		// 	package: Fortmatic, // required
-		// 	options: {
-		// 		key: "pk_live_1FEDAA23B87A151D" // required
-		// 	}
-		// }
+		fortmatic: {
+			package: Fortmatic, // required
+			options: {
+				key: "pk_live_1FEDAA23B87A151D" // required
+			}
+		}
 	};
 	const web3Modal = new Web3Modal({
 		network: "mainnet", // optional
